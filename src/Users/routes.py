8f8b13780c8d23a49f2488bc56de7fb4caa68eec
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2021-10-21 15:26:32 -0400

    Update routes.py

--- src/Users/routes.py
+++ src/Users/routes.py
@@ -1,1 +1,16 @@
+from fastapi import APIRouter 
+from schema import LoginUser 
 
+router = APIRouter()
+
+@router.get('/user', tage=['users'])
+async def get_user():
+  return {'name': 'asdf', 'email': 'asdf@asdf.ca'}
+
+@router.post('/login')
+async def login_user(user_creds: LoginUser):
+  return {}
+
+@router.post('/logout')
+async def logout_user():
+  return {}
