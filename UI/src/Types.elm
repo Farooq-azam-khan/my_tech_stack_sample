958ec5ad146851d036d718554478f6423368a08a
Farooq <farooq1.khan@ryerson.ca>
2021-12-19 14:04:25 -0500

    brain storming

--- UI/src/Types.elm
+++ UI/src/Types.elm
@@ -6,6 +6,23 @@ import Http
 import RemoteData exposing (..)
 import Http.Detailed
 
+type alias Token = String 
+
+type alias Login = 
+    { name : String
+    , password : String
+    }
+type alias Register = 
+    { name : String
+    , password : String
+    , email    : String
+    }
+
+type alias User = 
+    { name : String
+    , email : String
+    , token: Token 
+    }
 
 -- MODEL
 type alias Model = 
