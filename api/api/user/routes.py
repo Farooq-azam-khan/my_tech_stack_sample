462fbd907016a84ed9432112d68cd4585d80e738
farooq-azam-khan <farooq1.khan@ryerson.ca>
2021-12-19 13:57:35 -0500

    wrote post requests and get requests

--- api/api/user/routes.py
+++ api/api/user/routes.py
@@ -106,3 +106,27 @@ router = APIRouter(
 )
 
 
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.name}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+
+@router.get("/me/todos/")
+async def read_own_todos(current_user: User = Depends(get_current_active_user)):
+    return [{"todo_id": 1, "todo_value": "make pasta", "owner": current_user.name}]
