268f5eda760c017b3e3955da9873091f57e4bb76
farooq-azam-khan <farooq1.khan@ryerson.ca>
2021-12-19 13:56:27 -0500

    removed old routes

--- api/api/user/routes.py
+++ api/api/user/routes.py
@@ -61,20 +61,10 @@ def authenticate_user(fake_db, username: str, password: str):
     return user
 
 router = APIRouter(
-  prefix='/user', 
-  tags=['user'], 
-  dependencies=[], 
-  responses={404: {'description': 'Not Found'}}
+    prefix="/user",
+    tags=["user"],
+    dependencies=[],
+    responses={404: {"description": "Not Found"}},
 )
 
-@router.get('/user', tags=['users'])
-async def get_user():
-  return {'name': 'asdf', 'email': 'asdf@asdf.ca'}
 
-@router.post('/login')
-async def login_user(_: LoginUser):
-  return {}
-
-@router.post('/logout')
-async def logout_user():
-  return {}
