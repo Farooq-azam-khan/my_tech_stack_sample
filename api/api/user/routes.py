500a274d4debaafab0f97fb85b8994bdb502c352
farooq-azam-khan <farooq1.khan@ryerson.ca>
2021-12-19 13:52:29 -0500

    get_password_hash

--- api/api/user/routes.py
+++ api/api/user/routes.py
@@ -42,6 +42,9 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
 router = APIRouter(
   prefix='/user', 
   tags=['user'], 
