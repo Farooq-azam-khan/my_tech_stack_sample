6eac7a742b0b1c87a2c7ad018f20362cf193d597
farooq-azam-khan <farooq1.khan@ryerson.ca>
2021-12-19 13:56:56 -0500

    Update routes.py
    create_access_token
    get_current_user
    get_current_active_user

--- api/api/user/routes.py
+++ api/api/user/routes.py
@@ -60,6 +60,44 @@ def authenticate_user(fake_db, username: str, password: str):
         return False
     return user
 
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        name: str = payload.get("sub")
+        if name is None:
+            raise credentials_exception
+        token_data = TokenData(name=name)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.name)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
 router = APIRouter(
     prefix="/user",
     tags=["user"],
