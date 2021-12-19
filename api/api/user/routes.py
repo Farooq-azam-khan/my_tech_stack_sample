0eeb9e8d36f2c3339fd882bab61e7ca0f158ce8a
farooq-azam-khan <farooq1.khan@ryerson.ca>
2021-12-19 13:55:36 -0500

    authenticate_user

--- api/api/user/routes.py
+++ api/api/user/routes.py
@@ -52,6 +52,14 @@ def get_user(db, username: str):
         return UserInDB(**user_dict)
 
 
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
 router = APIRouter(
   prefix='/user', 
   tags=['user'], 
